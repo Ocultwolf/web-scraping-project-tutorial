8d8390e93691bb737daeddf528fb7cfa0db3db85
Ocultwolf <99327429+Ocultwolf@users.noreply.github.com>
2024-11-25 18:51:25 +0000

    ejercicio web scraping

--- src/app.py
+++ src/app.py
@@ -1,8 +1,31 @@
 import os
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
 import time
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+
+url = "https://ycharts.com/companies/TSLA/revenues"
+respuesta = requests.get(url)
+
+if respuesta.status_code == 200:
+    
+    soup = bs(respuesta.text, 'html.parser')
+
+    tables = soup.find_all('table')
+    for table in tables:
+        rows = table.find_all('tr')
+        # Obtener encabezados
+        headers = [header.text.strip() for header in rows[0].find_all(['th', 'td'])]
+    if headers == ["Date", "Value"]:
+        print("Tabla encontrada con las columnas 'Date' y 'Value'.")
+
+    # Iterar sobre las tablas y procesarlas
+    for i, table in enumerate(tables):
+        print(f"Tabla {i+1}:")
+        print(table.prettify())
+else:
+    print(f"Error al acceder a la página: {respuesta.status_code}")
